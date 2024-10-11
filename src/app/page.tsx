c12343bfb2dbf4c08f836f8418c3ae24ea5ec759
umesh kaushik <umeshkaushik@umeshs-MacBook-Air.local>
2024-10-11 11:30:54 +0530

    improved search section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,21 @@ import UndoRedo from '@/components/UndoRedo';
 
 export default function Home() {
   return (
-    <TaskProvider>
-      <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-4">Task Management</h1>
+
+    <TaskProvider >
+      <div className="container p-8">
+        <div className='flex justify-between'>
+        <h1 className="text-2xl font-bold mb-4">Task Management</h1>
+          <TaskSearch />
+          <TaskFilter />
+        </div>
         <TaskForm />
         <div className="flex flex-col sm:flex-row justify-between items-center mb-4">
-          <TaskFilter />
-          <TaskSearch />
         </div>
         <TaskList />
         <UndoRedo />
       </div>
     </TaskProvider>
+
   );
 }
--- src/components/TaskSearch.tsx
+++ src/components/TaskSearch.tsx
@@ -2,7 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import { useTaskContext } from '@/context/TaskContext';
 import useDebounce from '../hooks/useDebounce';
-
+import SearchIcon from '@/Assets/Search.svg';
+import Image from 'next/image';
 export default function TaskSearch() {
   const [searchTerm, setSearchTerm] = useState('');
   const { dispatch } = useTaskContext();
@@ -17,12 +18,17 @@ export default function TaskSearch() {
   };
 
   return (
-    <input
+    <div className='relative'>
+      <Image src={SearchIcon} className='absolute top-3 left-2' alt="search-icon" height={20} width={20} />
+      <input
       type="text"
       value={searchTerm}
       onChange={handleSearch}
       placeholder="Search tasks"
-      className="border p-2"
+      className="w-100 border text-gray-600 border-gray-400 px-12 py-2 rounded-full"
     />
+    </div>
+
+
   );
 }
